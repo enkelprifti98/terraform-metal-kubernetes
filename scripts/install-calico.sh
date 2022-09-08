e34d280da67a71eec92a1263bf24a2188fe123f4
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2022-09-08 14:55:26 -0400

    Update install-calico.sh

--- scripts/install-calico.sh
+++ scripts/install-calico.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install Calico
-kubectl apply -f https://docs.projectcalico.org/${calico_version}/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/${calico_version}/manifests/calico.yaml
 
 # Download the matching calicoctl version
 curl -L  \
-	https://github.com/projectcalico/calicoctl/releases/download/${calicoctl_version}/calicoctl \
+	https://github.com/projectcalico/calico/releases/download/${calicoctl_version}/calicoctl-linux-amd64 \
 	-o /usr/local/bin/calicoctl
 
 # Make it executable
