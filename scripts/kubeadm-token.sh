a0e27d6c93debb6cd0a5a9fe6f69536cdee69978
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2024-08-07 22:57:58 -0400

    Update kubeadm-token.sh

--- scripts/kubeadm-token.sh
+++ scripts/kubeadm-token.sh
@@ -5,10 +5,19 @@ set -e
 # Extract "host" argument from the input into HOST shell variable
 eval "$(jq -r '@sh "HOST=\(.host)"')"
 
+if ! ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i cluster-private-key.pem root@$HOST "test -e $HOME/.kube/config" ; then
+    # echo 'File "$HOME/.kube/config" is not there because the kubernetes cluster was not created, aborting.'
+    echo '{"command":"failed"}' | jq
+else {
+
 # Fetch the join command
 CMD=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
     -i cluster-private-key.pem \
-    root@$HOST kubeadm token create --ttl 0 --print-join-command)
+    root@$HOST kubeadm token create --ttl 0 --description terraform --print-join-command)
 
 # Produce a JSON object containing the join command
 jq -n --arg command "$CMD" '{"command":$command}'
+
+}
+
+fi
